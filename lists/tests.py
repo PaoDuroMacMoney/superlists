0349724d44bc8f50f6c88ce54c20824d5c896f00
PaoDuroMacMoney <mlopez.rodrigo@gmail.com>
2018-09-24 14:02:20 -0300

    inclusão de modelo no banco

--- lists/tests.py
+++ lists/tests.py
@@ -4,10 +4,11 @@ from django.http import HttpRequest
 from django.template.loader import render_to_string
 
 from lists.views import home_page
+from lists.models import Item
 
-class HomePageTest(TestCase):
+class HomePageTest(TestCase):	
 
-	
+
 	def test_defaul_returns_home_page(self):
 		found = resolve('/')
 		self.assertEqual(found.func, home_page)
@@ -22,3 +23,24 @@ class HomePageTest(TestCase):
 		response = self.client.post('/', data={'item_text': 'Uma nova tarefa'})
 		self.assertIn('Uma nova tarefa', response.content.decode())
 		self.assertTemplateUsed(response, 'home.html')
+
+
+class ItemModelTest(TestCase):
+
+
+	def test_saving_and_retrieving_items(self):
+		first_item = Item()
+		first_item.text = 'The first (ever) list item'
+		first_item.save()
+
+		second_item = Item()
+		second_item.text = 'Item the second'
+		second_item.save()
+
+		saved_items = Item.objects.all()
+		self.assertEqual(saved_items.count(), 2)
+
+		first_saved_item, second_saved_item = saved_items
+
+		self.assertEqual(first_saved_item.text, first_item.text)
+		self.assertEqual(second_saved_item.text, second_item.text)
