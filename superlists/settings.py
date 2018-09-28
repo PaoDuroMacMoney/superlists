4ddeef53f069a422f831a5071bf3d08efd784df7
PaoDuroMacMoney <mlopez.rodrigo@gmail.com>
2018-09-28 18:38:06 -0300

    incluindo layout com bootstrap

--- superlists/settings.py
+++ superlists/settings.py
@@ -31,13 +31,14 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'django.contrib.admin',
+    #'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-	'lists',
+    'lists',
+    'bootstrap4',
 ]
 
 MIDDLEWARE = [
@@ -119,3 +120,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
